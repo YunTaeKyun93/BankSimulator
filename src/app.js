2c9b6f68dd15abe86156b61f1358acda3793bb22
YunTaeKyun93 <ytk5495@gmail.com>
2022-10-28 12:18:55 +0900

    1028-1

--- src/app.js
+++ src/app.js
@@ -8,61 +8,59 @@ const NoMatchingCommandError = require("./noMatchingCommandError");
 const CreateUserParseCase = require("./Parser/createUserParseCase");
 const ShowUserParseCase = require("./Parser/showUserParseCase");
 const CreateBankParseCase = require("./Parser/createBankParseCase");
-const ShowBankParseCase = require('./Parser/showBankParseCase');
-const UserEarnsParseCase = require('./Parser/userEarnsParseCase');
-const UserUsesParseCase = require('./Parser/userUsesParceCase');
-const UserDepositsToBankParseCase = require('./Parser/userDepositsToBankParseCase');
-const UserWithdrawsFromBankParseCase = require('./Parser/userWithdrawsFromBankParseCase');
-const BankIssuesInterestOfDaysParseCase = require('./Parser/bankIssuesInterestOfDaysParseCase');
+const ShowBankParseCase = require("./Parser/showBankParseCase");
+const UserEarnsParseCase = require("./Parser/userEarnsParseCase");
+const UserUsesParseCase = require("./Parser/userUsesParceCase");
+const UserDepositsToBankParseCase = require("./Parser/userDepositsToBankParseCase");
+const UserWithdrawsFromBankParseCase = require("./Parser/userWithdrawsFromBankParseCase");
+const BankIssuesInterestOfDaysParseCase = require("./Parser/bankIssuesInterestOfDaysParseCase");
 
 const Test = require("./test");
-
-
 
 class App {
   getParseCase() {
     return [
-        new CreateUserParseCase(), 
-        new ShowUserParseCase(),
-        new CreateBankParseCase(),
-        new ShowBankParseCase(),
-        new UserEarnsParseCase(),
-        new UserUsesParseCase(),
-        new BankIssuesInterestOfDaysParseCase(),
-        new UserDepositsToBankParseCase(),
-        new UserWithdrawsFromBankParseCase(),
+      new CreateUserParseCase(),
+      new ShowUserParseCase(),
+      new CreateBankParseCase(),
+      new ShowBankParseCase(),
+      new UserEarnsParseCase(),
+      new UserUsesParseCase(),
+      new BankIssuesInterestOfDaysParseCase(),
+      new UserDepositsToBankParseCase(),
+      new UserWithdrawsFromBankParseCase()
     ];
   }
 
-  tryParseAndRun(command) {
-    const action = this.parser.getAction(command);
-    action.context = this.context; // 이 부분 모르겠음
-    action.run(); // 이 부분 모르겠음
-  }
-  parseAndRun(command) {
-    try {
-      this.tryParseAndRun(command);
-    } catch (error) {
-      if (error instanceof NoMatchingCommandError) {
-        console.error("잘못된 명령어를 입력하셨습니다. 방금 입력한 명령어:");
-        console.log(e.command);
-        return;
-      }
-      throw error;
-    }
-  }
+  //   tryParseAndRun(command) {
+  //     const action = this.parser.getAction(command);
+  //     action.context = this.context; // 이 부분 모르겠음
+  //     action.run(); // 이 부분 모르겠음
+  //   }
+  //   parseAndRun(command) {
+  //     try {
+  //       this.tryParseAndRun(command);
+  //     } catch (error) {
+  //       if (error instanceof NoMatchingCommandError) {
+  //         console.error("잘못된 명령어를 입력하셨습니다. 방금 입력한 명령어:");
+  //         console.log(e.command);
+  //         return;
+  //       }
+  //       throw error;
+  //     }
+  //   }
   run() {
     this.readLine = new ReadLine();
     this.context = new Context();
     this.parser = new Parser(this.getParseCase());
+    this.parseAndRun = new ParseAndRun();
 
     const readLine = this.readLine;
     const context = this.context;
     const parser = this.parser;
+    const parseAndRun = this.ParseAndRun;
 
-    readLine.registerCallback((text) => this.parseAndRun(text));
-
-    new Test(this).run();
+    readLine.registerCallback((text) => parseAndRun.parseAndRun(text));
   }
 }
 
---
+++ src/parseAndRun.js
@@ -0,0 +1,5 @@
+class TryParseAndRun{
+
+}
+
+module.export = TryParseAndRun;
